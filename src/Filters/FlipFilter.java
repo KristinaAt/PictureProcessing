a3c043e1e65db9798fc87702490c11889bedab0b
kristina <ka4518@ic.ac.uk>
2019-08-21 14:25:29 +0300

    [Krisi+Ghush] Added flipping filters to flip vertically and horizontally as well as SwapPixels helper method

---
+++ src/Filters/FlipFilter.java
@@ -0,0 +1,33 @@
+package Filters;
+
+import java.awt.image.BufferedImage;
+
+public class FlipFilter {
+  public static BufferedImage FlipHorizontalFilter(BufferedImage img){
+    int width = img.getWidth();
+    int height = img.getHeight();
+    for(int x = 0; x < (width / 2); x++){
+      for(int y = 0; y < height; y++){
+        SwapPixels(x, y, (width - x - 1), y, img);
+      }
+    }
+    return img;
+  }
+
+  public static BufferedImage FlipVerticalFilter(BufferedImage img){
+    int width = img.getWidth();
+    int height = img.getHeight();
+    for(int y = 0; y < (height/2); y++){
+      for(int x = 0; x < width; x++){
+        SwapPixels(x, y, x, (height - y - 1), img);
+      }
+    }
+    return img;
+  }
+
+  private static void SwapPixels(int x1, int y1, int x2, int y2, BufferedImage img){
+    int RGBtemp = img.getRGB(x1, y1);
+    img.setRGB(x1, y1, img.getRGB(x2, y2));
+    img.setRGB(x2, y2, RGBtemp);
+  }
+}
--- src/PictureProcessing.java
+++ src/PictureProcessing.java
@@ -6,7 +6,7 @@ public class PictureProcessing {
   public static void main(String[] args) {
 
     BufferedImage img = OpenSaveImage.openImage(args[0]);
-    img = ChangeBrightnessFilter.BrightnessFilter(img, 0.7);
+    img = FlipFilter.FlipHorizontalFilter(img);
     OpenSaveImage.SaveImage(img, args[1]);
   }
 }
